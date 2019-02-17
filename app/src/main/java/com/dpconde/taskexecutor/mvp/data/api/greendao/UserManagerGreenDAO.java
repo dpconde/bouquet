8224d880138d22133d21b8d574d15c71f5e535c8
David <dpconde.me@gmail.com>
2019-02-17 23:28:36 +0100

    test commit

--- app/src/main/java/com/dpconde/taskexecutor/mvp/data/api/greendao/UserManagerGreenDAO.java
+++ app/src/main/java/com/dpconde/taskexecutor/mvp/data/api/greendao/UserManagerGreenDAO.java
@@ -2,17 +2,27 @@ package com.dpconde.taskexecutor.mvp.data.api.greendao;
 
 import com.dpconde.taskexecutor.mvp.data.api.Callback;
 import com.dpconde.taskexecutor.mvp.data.api.UserManager;
+import com.dpconde.taskexecutor.mvp.data.model.DaoSession;
 import com.dpconde.taskexecutor.mvp.data.model.User;
+import com.dpconde.taskexecutor.mvp.data.model.UserDao;
 import com.google.common.hash.Hashing;
 
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
+import java.util.List;
 
 /**
  * Created by dpconde on 28/9/18.
  */
 
 public class UserManagerGreenDAO implements UserManager {
+
+
+    DaoSession daoSession; 
+
+    public UserManagerGreenDAO(DaoSession daoSession) {
+        this.daoSession = daoSession;
+    }
+
 
 
     @Override
@@ -30,6 +40,7 @@ public class UserManagerGreenDAO implements UserManager {
         return null;
     }
 
+
     @Override
     public User createUser(User user) {
         return null;
@@ -40,7 +51,7 @@ public class UserManagerGreenDAO implements UserManager {
     public User doLogin(String userCode, String password) {
 
         //The user must be persisted in order to do login
-        User user = this.getUserByUsercode(userCode);
+        User user = this.getUserByUserCode(userCode);
 
         if(user == null){
             return null;
@@ -58,7 +69,12 @@ public class UserManagerGreenDAO implements UserManager {
         return null;
     }
 
-    private User getUserByUsercode(String usercode){
-        return null;
+    private User getUserByUserCode(String userCode){
+
+        List<User> users = daoSession.getUserDao().queryBuilder()
+                .where(UserDao.Properties.UserCode.eq(userCode))
+                .list();
+
+        return users.isEmpty() ? null : users.get(0);
     }
 }
